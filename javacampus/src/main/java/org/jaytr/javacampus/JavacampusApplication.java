22d3fd09774567e237a917baa944f3253e63c304
김선재 <masterpc1013@gmail.com>
2022-07-25 21:08:38 +0900

    JavaCampus with Spring Boot First Commit

---
+++ javacampus/src/main/java/org/jaytr/javacampus/JavacampusApplication.java
@@ -0,0 +1,13 @@
+package org.jaytr.javacampus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavacampusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavacampusApplication.class, args);
+	}
+
+}
---
+++ javacampus/src/test/java/org/jaytr/javacampus/JavacampusApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jaytr.javacampus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavacampusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
